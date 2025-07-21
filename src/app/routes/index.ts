295359688ee960cea817bca1e87eec59eade46ef
Nesat Nayem <nesatnayem042@gmail.com>
2025-07-21 06:37:55 +0600

    ready bigsell apis

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,44 +5,23 @@ import { bannerRouter } from "../modules/banner/banner.routes";
 import { cartRouter } from "../modules/cart/cart.routes";
 import { orderRouter } from "../modules/order/order.routes";
 import { aiRouter } from "../modules/ai/ai.routes";
-import { tableRouter } from "../modules/table/table.routes";
-import { paymentRouter } from "../modules/payment/payment.routes";
 import { contractRouter } from "../modules/contact/contract.routes";
-import { hotelRouter } from "../modules/hotel/hotel.routes";
-import { kycRouter } from "../modules/kyc/kyc.routes";
 import { saveCardRouter } from "../modules/savecard/savecard.routes";
 import { faqRouter } from "../modules/faq/faq.routes";
-import { offerRouter } from "../modules/offer/offer.routes";
 import { privacyPolicyRouter } from "../modules/privacy-policy/privacy-policy.routes";
-import { tableBookingRouter } from "../modules/table-booking/table-booking.routes";
-import { featureOfferRouter } from "../modules/feature-offer/feature-offer.routes";
 import { TermsConditionRouter } from "../modules/terms-condition/terms-condition.routes";
 import { helpSupportRouter } from "../modules/help-support/help-support.routes";
 import { blogRouter } from "../modules/blog/blog.routes";
-import { mustTryRouter } from "../modules/musttry/musttry.routes";
 import { uploadRouter } from "../modules/upload/upload.routes";
-import { staffRouter } from "../modules/staff/staff.routes";
-import { staffOrderRouter } from "../modules/staff/staff.order.routes";
-import { pricingRouter } from "../modules/pricing/pricing.routes";
-import { hotelBookingSettingsRouter } from "../modules/hotel-booking-settings/hotel-booking-settings.routes";
 import { bookmarkRouter } from "../modules/bookmark/bookmark.routes";
-import { qrcodeRouter } from "../modules/qrcode/qrcode.routes";
-import { adminStaffRouter } from "../modules/admin-staff/admin-staff.routes";
 import { couponRouter } from "../modules/coupon/coupon.routes";
 import { reportRouter } from "../modules/report/report.routes";
-import activityRouter from "../modules/activity/activity.routes";
-import { placesRouter } from "../modules/places/places.routes";
 
 const router = Router();
 const moduleRoutes = [
   {
     path: "/auth",
     route: authRouter,
-  },
-
-  {
-    path: "/activities",
-    route: activityRouter,
   },
 
   {
@@ -61,11 +40,6 @@ const moduleRoutes = [
   },
 
   {
-    path: "/qrcodes",
-    route: qrcodeRouter,
-  },
-
-  {
     path: "/cart",
     route: cartRouter,
   },
@@ -76,25 +50,6 @@ const moduleRoutes = [
   {
     path: "/ai",
     route: aiRouter,
-  },
-  {
-    path: "/qr-scanner",
-    route: tableRouter,
-  },
-
-  {
-    path: "/payments",
-    route: paymentRouter,
-  },
-
-  {
-    path: "/hotels",
-    route: hotelRouter,
-  },
-
-  {
-    path: "/kyc",
-    route: kycRouter,
   },
 
   {
@@ -108,22 +63,8 @@ const moduleRoutes = [
   },
 
   {
-    path: "/offers",
-    route: offerRouter,
-  },
-
-  {
     path: "/privacy-policy",
     route: privacyPolicyRouter,
-  },
-
-  {
-    path: "/table-bookings",
-    route: tableBookingRouter,
-  },
-  {
-    path: "/feature-offers",
-    route: featureOfferRouter,
   },
 
   {
@@ -142,39 +83,13 @@ const moduleRoutes = [
   },
 
   {
-    path: "/must-try",
-    route: mustTryRouter,
-  },
-
-  {
     path: "/uploads",
     route: uploadRouter,
   },
 
   {
-    path: "/staff",
-    route: staffRouter,
-  },
-  {
-    path: "/staff-orders",
-    route: staffOrderRouter,
-  },
-  {
-    path: "/pricing",
-    route: pricingRouter,
-  },
-
-  {
-    path: "/hotel-booking-settings",
-    route: hotelBookingSettingsRouter,
-  },
-  {
     path: "/bookmarks",
     route: bookmarkRouter,
-  },
-  {
-    path: "/admin-staff",
-    route: adminStaffRouter,
   },
 
   {
@@ -184,10 +99,6 @@ const moduleRoutes = [
   {
     path: "/reports",
     route: reportRouter,
-  },
-  {
-    path: "/places",
-    route: placesRouter,
   },
 ];
 
