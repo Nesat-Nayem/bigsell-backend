c49bcf83f674fd8c6773919fc682c8f5a433707d
Nesat Nayem <nesatnayem042@gmail.com>
2025-09-29 11:17:50 +0600

    cros update

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import cors, { CorsOptions } from "cors";
 import router from "./app/routes";
 import notFound from "./app/middlewares/notFound";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
@@ -23,14 +23,27 @@ const getAllowedOrigins = () => {
   return [...defaultOrigins, ...productionOrigins, ...envOrigins];
 };
 
-const corsOptions = {
-  origin: true,
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    // Reflect the request origin to support credentials from any frontend
+    callback(null, true);
+  },
   credentials: true,
-  optionsSuccessStatus: 200,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+  ],
+  exposedHeaders: ["Content-Disposition"],
+  optionsSuccessStatus: 204,
 };
 
 // CORS middleware - Apply before other middlewares
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 // Body parsers
 app.use(express.json({ limit: "50mb" }));
